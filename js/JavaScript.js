b1769a1ba3e4d626f5591efbd5a3f21bdbfd84cb
malesani <alejandroMalesani@hotmail.com>
2021-04-09 15:35:27 +0200

    first

---
+++ js/JavaScript.js
@@ -0,0 +1,13 @@
+//stard programm
+var stard = prompt ("scrive ok per iniziare")
+if(stard == "ok"){
+  for(i=0; i < 100; i++){
+    
+  }
+}else{
+  alert("riprova")
+}
+
+var numeri = [0]
+
+console.log(all)
